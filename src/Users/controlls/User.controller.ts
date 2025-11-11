224f58229eaaf4b6cdfb7d1806a17fa33fa7060e
Alexsandro-J-Ludwig <alexsandrojacksomlduwigvasco@gmail.com>
2025-11-10 22:15:31 -0300

    feat: implement createUser method in UserController with async error handling

--- src/Users/controlls/User.controller.ts
+++ src/Users/controlls/User.controller.ts
@@ -1,8 +1,12 @@
 import type { Request, Response } from "express";
 import { UserDTO } from "../DTOs/User.dto.ts";
+import { asyncHandler } from "../../utils/AsyncHandler.ts";
+import { UserService } from "../services/User.service.ts";
 
 class UserController {
-    static createUser(req: Request, res: Response){
-        
-    }
+    static createUser = asyncHandler(async (req: Request, res: Response) => {
+        const dto = UserDTO.fromRequest(req.body);
+        const user = await UserService.createUser(dto);
+        res.status(201).json({ sucess: true, data: user });
+    });
 }
