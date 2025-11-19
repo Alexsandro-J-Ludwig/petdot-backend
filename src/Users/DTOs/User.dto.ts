23ce4d5656fadda98dcc314e904a312078f5e3dd
Ale-ludw <alexsandrojacksomludwigvasco@gmail.com>
2025-11-19 17:00:53 -0300

    fix: image dto was fixed

--- src/Users/DTOs/User.dto.ts
+++ src/Users/DTOs/User.dto.ts
@@ -6,7 +6,6 @@ class UserDTO {
     public readonly email: string,
     public readonly pass: string,
     public readonly celular: string,
-    public readonly imageURL: string | null
   ) {
     UserValidator.validateAll({ name, email, pass, celular });
   }
@@ -17,7 +16,6 @@ class UserDTO {
       body.email,
       body.pass,
       body.celular,
-      body.imageURL
     );
   }
 }
